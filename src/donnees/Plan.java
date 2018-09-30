e1485ed62b2be443ab056e0d9ea8c9e919a71028
remig <remig@LAPTOP-0MI3F697>
2018-09-30 19:09:00 +0200

    affiche segment du modèle (non testé), z non pris en charge

--- src/donnees/Plan.java
+++ src/donnees/Plan.java
@@ -28,11 +28,19 @@ public class Plan extends Application {
 		Group root = new Group();
 		Canvas canvas = new Canvas(300, 250);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
+		drawModele(gc);
 		root.getChildren().add(canvas);
 		Scene scene = new Scene(root);
 		Stage s = new Stage();
 		s.setScene(scene);
 		s.setTitle("3D scene");
 		s.show();	
-	}	
+	}
+	
+	public void drawModele(GraphicsContext gc) {
+		for(int i=0;i<m.getSegments().size();i++) {
+			Segment seg = m.getSegments().get(i);
+			gc.strokeLine(seg.getPointA().getX(), seg.getPointA().getY(), seg.getPointB().getX(), seg.getPointB().getY());
+		}
+	}
 }
