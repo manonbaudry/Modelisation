730a26d8843121ead671fd415095edefd853c05a
Florian PRUNIER <florian.prunier@etudiant.univ-lille1.fr>
2018-10-15 11:00:23 +0200

    rotation mouse

--- src/donnees/Plan.java
+++ src/donnees/Plan.java
@@ -19,7 +19,7 @@ public class Plan extends Application {
 
 	private Modele m;
 	private Canvas canvas;
-	private int x ;
+	private int x;
 	private int y;
 
 	public Plan(Modele m, String image) {
@@ -34,218 +34,223 @@ public class Plan extends Application {
 	public void start(Stage s) throws Exception {
 		Group root = new Group();
 		HBox commande = new HBox();
-		
+
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
+
 		Button gauche = new Button("<-");
-		gauche.setOnAction(e->{
+		gauche.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(-10, 0, 0), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(-10, 0, 0),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button droite = new Button("->");
-		droite.setOnAction(e->{
+		droite.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(10, 0, 0), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(10, 0, 0),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button haut = new Button("↑");
-		haut.setOnAction(e->{
+		haut.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(0, -10, 0), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(0, -10, 0),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button bas = new Button("↓");
-		bas.setOnAction(e->{
+		bas.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(0, 10, 0), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.translation(CalculMatrice.vecteur(0, 10, 0),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button plus = new Button("-");
-		plus.setOnAction(e->{
+		plus.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(0.5, 0.5, 0.5), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(0.5, 0.5, 0.5),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button moins = new Button("+");
-		moins.setOnAction(e->{
+		moins.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(2, 2, 2), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+				Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(2, 2, 2),
+						CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button rotateGauche = new Button("↶");
-		rotateGauche.setOnAction(e->{
+		rotateGauche.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.rotationZ(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), Math.PI/16);
+				Matrice mt = CalculMatrice.rotationZ(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), Math.PI / 16);
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
 		Button rotateDroite = new Button("↷");
-		rotateDroite.setOnAction(e->{
+		rotateDroite.setOnAction(e -> {
 			for (int i = 0; i < m.getPoints().size(); i++) {
 				Point p = m.getPoints().get(i);
-				Matrice mt = CalculMatrice.rotationZ(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), -Math.PI/16);
+				Matrice mt = CalculMatrice.rotationZ(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()),
+						-Math.PI / 16);
 				p.setX(mt.getCoord(0, 0));
 				p.setY(mt.getCoord(1, 0));
 				p.setZ(mt.getCoord(2, 0));
 			}
-			m.triFaces(m.getFaces());
 			drawModele(gc);
 		});
-		
-		canvas.setOnScroll(e->{
+
+		canvas.setOnScroll(e -> {
 			if (e.getDeltaY() > 0) {
 				for (int i = 0; i < m.getPoints().size(); i++) {
 					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(2, 2, 2), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+					Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(2, 2, 2),
+							CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 					p.setX(mt.getCoord(0, 0));
 					p.setY(mt.getCoord(1, 0));
 					p.setZ(mt.getCoord(2, 0));
 				}
-				m.triFaces(m.getFaces());
 				drawModele(gc);
-			}else {
+			} else {
 				for (int i = 0; i < m.getPoints().size(); i++) {
 					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(0.5, 0.5, 0.5), CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
+					Matrice mt = CalculMatrice.chgmtEchelle(CalculMatrice.vecteur(0.5, 0.5, 0.5),
+							CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()));
 					p.setX(mt.getCoord(0, 0));
 					p.setY(mt.getCoord(1, 0));
 					p.setZ(mt.getCoord(2, 0));
 				}
-				m.triFaces(m.getFaces());
 				drawModele(gc);
 			}
 		});
-		
-		canvas.setOnMouseClicked(e->{
+
+		canvas.setOnMouseClicked(e -> {
 			x = (int) e.getX();
 			y = (int) e.getY();
 		});
-		
-		canvas.setOnMouseDragged(e->{
-			float xTemp = (float)e.getX();
+
+		canvas.setOnMouseDragged(e -> {
+			float xTemp = (float) e.getX();
 			int nb = 1;
-			if ((xTemp-x)<0) {
+			if ((xTemp - x) < 0) {
 				nb = -1;
 			}
-			float yTemp = (float)e.getY();
+			float yTemp = (float) e.getY();
 			int nbY = 1;
-			if ((yTemp-y)<0) {
+			if ((yTemp - y) < 0) {
 				nbY = -1;
 			}
-			if (nb > 0 && xTemp != x) {
-				for (int i = 0; i < m.getPoints().size(); i++) {
-					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.rotationY(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), Math.PI/32);
-					p.setX(mt.getCoord(0, 0));
-					p.setY(mt.getCoord(1, 0));
-					p.setZ(mt.getCoord(2, 0));
+			if (e.isPrimaryButtonDown()) {
+				if (nb > 0 && xTemp != x) {
+					for (int i = 0; i < m.getPoints().size(); i++) {
+						Point p = m.getPoints().get(i);
+						Matrice mt = CalculMatrice.rotationY(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()),
+								Math.PI / 32);
+						p.setX(mt.getCoord(0, 0));
+						p.setY(mt.getCoord(1, 0));
+						p.setZ(mt.getCoord(2, 0));
+					}
+					drawModele(gc);
+				} else if (xTemp != x) {
+					for (int i = 0; i < m.getPoints().size(); i++) {
+						Point p = m.getPoints().get(i);
+						Matrice mt = CalculMatrice.rotationY(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()),
+								-Math.PI / 32);
+						p.setX(mt.getCoord(0, 0));
+						p.setY(mt.getCoord(1, 0));
+						p.setZ(mt.getCoord(2, 0));
+					}
+					drawModele(gc);
 				}
-				m.triFaces(m.getFaces());
-				drawModele(gc);
-			}else if(xTemp != x) {
-				for (int i = 0; i < m.getPoints().size(); i++) {
-					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.rotationY(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), -Math.PI/32);
-					p.setX(mt.getCoord(0, 0));
-					p.setY(mt.getCoord(1, 0));
-					p.setZ(mt.getCoord(2, 0));
+			} else {
+				if (nbY > 0 && yTemp != y) {
+					for (int i = 0; i < m.getPoints().size(); i++) {
+						Point p = m.getPoints().get(i);
+						Matrice mt = CalculMatrice.rotationX(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()),
+								Math.PI / 32);
+						p.setX(mt.getCoord(0, 0));
+						p.setY(mt.getCoord(1, 0));
+						p.setZ(mt.getCoord(2, 0));
+					}
+					drawModele(gc);
+				} else if (yTemp != y) {
+					for (int i = 0; i < m.getPoints().size(); i++) {
+						Point p = m.getPoints().get(i);
+						Matrice mt = CalculMatrice.rotationX(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()),
+								-Math.PI / 32);
+						p.setX(mt.getCoord(0, 0));
+						p.setY(mt.getCoord(1, 0));
+						p.setZ(mt.getCoord(2, 0));
+					}
+					drawModele(gc);
 				}
-				m.triFaces(m.getFaces());
-				drawModele(gc);
-			}
-			if (nbY > 0 && yTemp != y) {
-				for (int i = 0; i < m.getPoints().size(); i++) {
-					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.rotationX(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), Math.PI/32);
-					p.setX(mt.getCoord(0, 0));
-					p.setY(mt.getCoord(1, 0));
-					p.setZ(mt.getCoord(2, 0));
-				}
-				m.triFaces(m.getFaces());
-				drawModele(gc);
-			}else if (yTemp != y) {
-				for (int i = 0; i < m.getPoints().size(); i++) {
-					Point p = m.getPoints().get(i);
-					Matrice mt = CalculMatrice.rotationX(CalculMatrice.vecteur(p.getX(), p.getY(), p.getZ()), -Math.PI/32);
-					p.setX(mt.getCoord(0, 0));
-					p.setY(mt.getCoord(1, 0));
-					p.setZ(mt.getCoord(2, 0));
-				}
-				m.triFaces(m.getFaces());
-				drawModele(gc);
 			}
 		});
-		
+
 		commande.getChildren().addAll(gauche, haut, moins, rotateGauche, rotateDroite, plus, bas, droite);
-		
+
 		root.getChildren().addAll(canvas, commande);
 		Scene scene = new Scene(root);
 		m.triFaces(m.getFaces());
 		drawModele(gc);
 		s.setScene(scene);
 		s.setTitle("3D scene");
-		s.show();	
+		s.show();
 	}
-	
+
 	public void drawModele(GraphicsContext gc) {
+		m.triFaces(m.getFaces());
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		for (int i = 0; i < m.getFaces().size(); i++) {
 			double[] xPoints = m.getFaces().get(i).xPoints();
 			double[] yPoints = m.getFaces().get(i).yPoints();
 			for (int j = 0; j < yPoints.length; j++) {
-				yPoints[j]+=canvas.getHeight()/2;
+				yPoints[j] += canvas.getHeight() / 2;
 			}
 			for (int j = 0; j < xPoints.length; j++) {
-				xPoints[j]+=canvas.getWidth()/2;
+				xPoints[j] += canvas.getWidth() / 2;
 			}
-			gc.fillPolygon(xPoints, yPoints, 3);
+			gc.strokePolygon(xPoints, yPoints, 3);
 			gc.setFill(Color.GRAY);
 		}
-		for(int i=0;i<m.getSegments().size();i++) {
+		/*for (int i = 0; i < m.getSegments().size(); i++) {
 			Segment seg = m.getSegments().get(i);
-			gc.strokeLine(seg.getPointA().getX()+canvas.getWidth()/2, seg.getPointA().getY()+canvas.getHeight()/2, seg.getPointB().getX()+canvas.getWidth()/2, seg.getPointB().getY()+canvas.getHeight()/2);
-		}
+			gc.strokeLine(seg.getPointA().getX() + canvas.getWidth() / 2,
+					seg.getPointA().getY() + canvas.getHeight() / 2, seg.getPointB().getX() + canvas.getWidth() / 2,
+					seg.getPointB().getY() + canvas.getHeight() / 2);
+		}*/
 	}
 }
